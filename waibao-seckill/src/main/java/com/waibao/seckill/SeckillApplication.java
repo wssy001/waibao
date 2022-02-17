c9ee434f48435139efe6e0e71f7bd04ac0949ffc
Tyler Alex <peter003@foxmail.com>
2022-02-17 16:34:46 +0800

    新增 RocketMQ

--- waibao-seckill/src/main/java/com/waibao/seckill/SeckillApplication.java
+++ waibao-seckill/src/main/java/com/waibao/seckill/SeckillApplication.java
@@ -2,6 +2,7 @@ package com.waibao.seckill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @since 2022-02-17
  */
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SeckillApplication {
     public static void main(String[] args) {

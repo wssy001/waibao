d61d0fe92bbb4348b9db4e0351dd126c113a03dd
Tyler Alex <peter003@foxmail.com>
2022-02-17 15:15:00 +0800

    代码同步

---
+++ waibao-seckill/src/main/java/com/waibao/seckill/SeckillApplication.java
@@ -0,0 +1,19 @@
+package com.waibao.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * SeckillApplication
+ *
+ * @author alexpetertyler
+ * @since 2022-02-17
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class SeckillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+}

7908743d11f0a6b14e57fdf04794275add38d283
韦伟杰 <atmzdfmj@outlook.com>
2022-02-17 13:48:23 +0800

    zuul换成gateway，nacos注册成功但不显示服务

---
+++ waibao-gateway/src/main/java/com/waibao/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.waibao.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: wwj
+ * @Date: 2022/2/17 10:57
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}

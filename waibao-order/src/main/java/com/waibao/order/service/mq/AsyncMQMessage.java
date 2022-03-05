897c46c6197619b908f23dc13821117a24545fb9
Tyler Alex <peter003@foxmail.com>
2022-03-06 02:13:02 +0800

    新增 OrderCheckConsumer 完善代码逻辑

--- waibao-order/src/main/java/com/waibao/order/service/mq/AsyncMQMessage.java
+++ waibao-order/src/main/java/com/waibao/order/service/mq/AsyncMQMessage.java
@@ -9,6 +9,7 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.common.message.Message;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
  * @since 2022/3/4
  */
 @Slf4j
+@Async
 @Service
 @RequiredArgsConstructor
 public class AsyncMQMessage {
@@ -46,8 +48,29 @@ public class AsyncMQMessage {
         }
     }
 
+    public void sendDelayedMessage(DefaultMQProducer producer, Message message, int delayedLevel) {
+        message.setTags(message.getTags() + "Check");
+        message.setDelayTimeLevel(delayedLevel);
+        String msgId = message.getKeys();
+        SendResult send;
+        try {
+            send = producer.send(message, SelectSingleMessageQueue.selectFirst(producer, message.getTopic()));
+        } catch (Exception e) {
+            log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", msgId, "producer发送失败", "等待延迟补偿结果");
+            sendMqMsgCompensation(message, e.getMessage());
+            return;
+        }
+
+        if (!send.getSendStatus().equals(SendStatus.SEND_OK)) {
+            String name = send.getSendStatus().name();
+            log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", msgId, name, "等待延迟补偿结果");
+            sendMqMsgCompensation(message, name);
+        } else {
+            log.info("******AsyncMQMessage.sendDelayedMessage：消息id：{} 发送成功", msgId);
+        }
+    }
+
     public void sendMessage(DefaultMQProducer producer, List<Message> messages) {
-        // TODO 发送延时消息，确认操作
         messages.parallelStream()
                 .collect(Collectors.groupingBy(Message::getTopic))
                 .forEach((k, v) -> {
@@ -68,6 +91,36 @@ public class AsyncMQMessage {
                     } else {
                         v.parallelStream()
                                 .forEach(message -> log.info("******AsyncMQMessage.sendMessage：消息id：{} 发送成功", message.getKeys()));
+                    }
+                });
+    }
+
+    //    1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
+    public void sendDelayedMessage(DefaultMQProducer producer, List<Message> messages, int delayedLevel) {
+        messages.parallelStream()
+                .peek(message -> {
+                    message.setTopic(message.getTopic() + "Check");
+                    message.setDelayTimeLevel(delayedLevel);
+                })
+                .collect(Collectors.groupingBy(Message::getTopic))
+                .forEach((k, v) -> {
+                    SendResult send;
+                    try {
+                        send = producer.send(v, SelectSingleMessageQueue.selectFirst(producer, k));
+                    } catch (Exception e) {
+                        sendMqMsgCompensation(v, e.getMessage());
+                        v.parallelStream()
+                                .forEach(message -> log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", message.getKeys(), "producer发送失败", "等待延迟补偿结果"));
+                        return;
+                    }
+                    if (!send.getSendStatus().equals(SendStatus.SEND_OK)) {
+                        String name = send.getSendStatus().name();
+                        sendMqMsgCompensation(v, name);
+                        v.parallelStream()
+                                .forEach(message -> log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", message.getKeys(), name, "等待延迟补偿结果"));
+                    } else {
+                        v.parallelStream()
+                                .forEach(message -> log.info("******AsyncMQMessage.sendDelayedMessage：消息id：{} 发送成功", message.getKeys()));
                     }
                 });
     }
--- waibao-seckill/src/main/java/com/waibao/seckill/service/mq/AsyncMQMessage.java
+++ waibao-seckill/src/main/java/com/waibao/seckill/service/mq/AsyncMQMessage.java
@@ -9,7 +9,11 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.common.message.Message;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * SendAsyncMQMessage
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
  * @since 2022/3/4
  */
 @Slf4j
+@Async
 @Service
 @RequiredArgsConstructor
 public class AsyncMQMessage {
@@ -43,6 +48,106 @@ public class AsyncMQMessage {
         }
     }
 
+    public void sendDelayedMessage(DefaultMQProducer producer, Message message, int delayedLevel) {
+        message.setTags(message.getTags() + "Check");
+        message.setDelayTimeLevel(delayedLevel);
+        String msgId = message.getKeys();
+        SendResult send;
+        try {
+            send = producer.send(message, SelectSingleMessageQueue.selectFirst(producer, message.getTopic()));
+        } catch (Exception e) {
+            log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", msgId, "producer发送失败", "等待延迟补偿结果");
+            sendMqMsgCompensation(message, e.getMessage());
+            return;
+        }
+
+        if (!send.getSendStatus().equals(SendStatus.SEND_OK)) {
+            String name = send.getSendStatus().name();
+            log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", msgId, name, "等待延迟补偿结果");
+            sendMqMsgCompensation(message, name);
+        } else {
+            log.info("******AsyncMQMessage.sendDelayedMessage：消息id：{} 发送成功", msgId);
+        }
+    }
+
+    public void sendMessage(DefaultMQProducer producer, List<Message> messages) {
+        messages.parallelStream()
+                .collect(Collectors.groupingBy(Message::getTopic))
+                .forEach((k, v) -> {
+                    SendResult send;
+                    try {
+                        send = producer.send(v, SelectSingleMessageQueue.selectFirst(producer, k));
+                    } catch (Exception e) {
+                        sendMqMsgCompensation(v, e.getMessage());
+                        v.parallelStream()
+                                .forEach(message -> log.error("******AsyncMQMessage.sendMessage：消息id：{} 原因：{} 处理:{}", message.getKeys(), "producer发送失败", "等待延迟补偿结果"));
+                        return;
+                    }
+                    if (!send.getSendStatus().equals(SendStatus.SEND_OK)) {
+                        String name = send.getSendStatus().name();
+                        sendMqMsgCompensation(v, name);
+                        v.parallelStream()
+                                .forEach(message -> log.error("******AsyncMQMessage.sendMessage：消息id：{} 原因：{} 处理:{}", message.getKeys(), name, "等待延迟补偿结果"));
+                    } else {
+                        v.parallelStream()
+                                .forEach(message -> log.info("******AsyncMQMessage.sendMessage：消息id：{} 发送成功", message.getKeys()));
+                    }
+                });
+    }
+
+    //    1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
+    public void sendDelayedMessage(DefaultMQProducer producer, List<Message> messages, int delayedLevel) {
+        messages.parallelStream()
+                .peek(message -> {
+                    message.setTopic(message.getTopic() + "Check");
+                    message.setDelayTimeLevel(delayedLevel);
+                })
+                .collect(Collectors.groupingBy(Message::getTopic))
+                .forEach((k, v) -> {
+                    SendResult send;
+                    try {
+                        send = producer.send(v, SelectSingleMessageQueue.selectFirst(producer, k));
+                    } catch (Exception e) {
+                        sendMqMsgCompensation(v, e.getMessage());
+                        v.parallelStream()
+                                .forEach(message -> log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", message.getKeys(), "producer发送失败", "等待延迟补偿结果"));
+                        return;
+                    }
+                    if (!send.getSendStatus().equals(SendStatus.SEND_OK)) {
+                        String name = send.getSendStatus().name();
+                        sendMqMsgCompensation(v, name);
+                        v.parallelStream()
+                                .forEach(message -> log.error("******AsyncMQMessage.sendDelayedMessage：消息id：{} 原因：{} 处理:{}", message.getKeys(), name, "等待延迟补偿结果"));
+                    } else {
+                        v.parallelStream()
+                                .forEach(message -> log.info("******AsyncMQMessage.sendDelayedMessage：消息id：{} 发送成功", message.getKeys()));
+                    }
+                });
+    }
+
+    private void sendMqMsgCompensation(List<Message> messages, String exceptionMsg) {
+        List<MqMsgCompensation> collect = messages.parallelStream()
+                .map(message -> {
+                    MqMsgCompensation mqMsgCompensation = new MqMsgCompensation();
+                    mqMsgCompensation.setTags(message.getTags());
+                    mqMsgCompensation.setTopic(message.getTopic());
+                    mqMsgCompensation.setContent(new String(message.getBody()));
+                    String msgId = message.getKeys();
+                    mqMsgCompensation.setMsgId(msgId);
+                    mqMsgCompensation.setExceptionMsg(exceptionMsg);
+                    return mqMsgCompensation;
+                })
+                .collect(Collectors.toList());
+        try {
+            mqMsgCompensationService.saveOrUpdateBatch(collect);
+            messages.parallelStream()
+                    .forEach(message -> log.info("******AsyncMQMessage.sendMqMsgCompensation：补偿消息存储成功，消息id：{}", message.getKeys()));
+        } catch (Exception e) {
+            messages.parallelStream()
+                    .forEach(message -> log.info("******AsyncMQMessage.sendMqMsgCompensation：补偿消息存储失败，消息id：{}", message.getKeys()));
+        }
+    }
+
     private void sendMqMsgCompensation(Message message, String exceptionMsg) {
         MqMsgCompensation mqMsgCompensation = new MqMsgCompensation();
         mqMsgCompensation.setTags(message.getTags());
@@ -53,9 +158,9 @@ public class AsyncMQMessage {
         mqMsgCompensation.setExceptionMsg(exceptionMsg);
         try {
             mqMsgCompensationService.saveOrUpdate(mqMsgCompensation);
-            log.info("******AsyncMQMessage.sendMessage：补偿消息存储成功，消息id：{}", msgId);
+            log.info("******AsyncMQMessage.sendMqMsgCompensation：补偿消息存储成功，消息id：{}", msgId);
         } catch (Exception e) {
-            log.info("******AsyncMQMessage.sendMessage：补偿消息存储失败，消息id：{}", msgId);
+            log.info("******AsyncMQMessage.sendMqMsgCompensation：补偿消息存储失败，消息id：{}", msgId);
         }
     }
 }

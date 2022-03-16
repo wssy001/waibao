0d429a469215aef118b49beffbcb514eabdb4e00
Tyler Alex <peter003@foxmail.com>
2022-03-16 18:41:54 +0800

    1.提取Temp.lua

--- waibao-order/src/main/java/com/waibao/order/service/cache/OrderRetailerCacheService.java
+++ waibao-order/src/main/java/com/waibao/order/service/cache/OrderRetailerCacheService.java
@@ -5,9 +5,10 @@ import com.alibaba.fastjson.JSONArray;
 import com.waibao.order.entity.OrderRetailer;
 import com.waibao.util.base.RedisCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -32,57 +33,17 @@ public class OrderRetailerCacheService {
     @Resource
     private RedisTemplate<String, OrderRetailer> orderRetailerRedisTemplate;
 
-    private DefaultRedisScript<String> canalSync;
+    private RedisScript<String> canalSync;
     private SetOperations<String, OrderRetailer> setOperations;
-    private DefaultRedisScript<String> batchInsertOrderRetailer;
-    private DefaultRedisScript<String> batchDeleteOrderRetailer;
+    private RedisScript<String> batchInsertOrderRetailer;
+    private RedisScript<String> batchDeleteOrderRetailer;
 
     @PostConstruct
     void init() {
-        String batchInsertScript = "local key = KEYS[1]\n" +
-                "local orderRetailerList = {}\n" +
-                "for index, value in ipairs(ARGV) do\n" +
-                "    local orderRetailer = cjson.decode(value)\n" +
-                "    local count = tonumber(redis.call('SETNX', key .. orderRetailer['retailerId'], orderRetailer['orderId']))\n" +
-                "    if count == 0 then\n" +
-                "        table.insert(orderRetailerList, orderRetailer)\n" +
-                "    end\n" +
-                "end\n" +
-                "if table.maxn(orderRetailerList) == 0 then\n" +
-                "    return nil\n" +
-                "else\n" +
-                "    return cjson.encode(orderRetailerList)\n" +
-                "end";
-        String batchDeleteScript = "local key = KEYS[1]\n" +
-                "local orderRetailerList = {}\n" +
-                "for index, value in ipairs(ARGV) do\n" +
-                "    local orderRetailer = cjson.decode(value)\n" +
-                "    local count = tonumber(redis.call('SREM', key .. orderRetailer['retailerId'], orderRetailer['orderId']))\n" +
-                "    if count == 0 then\n" +
-                "        table.insert(orderRetailerList, orderRetailer)\n" +
-                "    end\n" +
-                "end\n" +
-                "if table.maxn(orderRetailerList) == 0 then\n" +
-                "    return nil\n" +
-                "else\n" +
-                "    return cjson.encode(orderRetailerList)\n" +
-                "end";
-        String canalSyncScript = "local key = KEYS[1]\n" +
-                "for index, value in ipairs(ARGV) do\n" +
-                "    local redisCommand = cjson.decode(value)\n" +
-                "    local orderRetailer = redisCommand['value']\n" +
-                "    key = '\"' .. string.gsub(key, '\"', '') .. orderRetailer['retailerId'] .. '\"'\n" +
-                "    if redisCommand['command'] == 'SET' then\n" +
-                "        orderRetailer['@type'] = 'com.waibao.order.entity.OrderRetailer'\n" +
-                "        redis.call('SET', key, cjson.encode(orderRetailer))\n" +
-                "    else\n" +
-                "        redis.call('DEL', key)\n" +
-                "    end\n" +
-                "end";
-        canalSync = new DefaultRedisScript<>(canalSyncScript);
         setOperations = orderRetailerRedisTemplate.opsForSet();
-        batchInsertOrderRetailer = new DefaultRedisScript<>(batchInsertScript, String.class);
-        batchDeleteOrderRetailer = new DefaultRedisScript<>(batchDeleteScript, String.class);
+        canalSync = RedisScript.of(new ClassPathResource("lua/canalSyncOrderRetailerScript.lua"), String.class);
+        batchInsertOrderRetailer = RedisScript.of(new ClassPathResource("lua/batchInsertOrderRetailerScript.lua"), String.class);
+        batchDeleteOrderRetailer = RedisScript.of(new ClassPathResource("lua/batchDeleteOrderRetailerScript.lua"), String.class);
     }
 
     public List<OrderRetailer> get(Long retailerId) {

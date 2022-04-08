96db7e6ec3340703020ec48d58df77588f66e492
Tyler Alex <peter003@foxmail.com>
2022-04-08 19:39:53 +0800

    修复Lua脚本Bug

--- waibao-seckill/src/main/resources/lua/getGoodsStatus.lua
+++ waibao-seckill/src/main/resources/lua/getGoodsStatus.lua
@@ -1,7 +1,7 @@
 local key = KEYS[1]
 local goodsId = tostring(ARGV[1])
 
-if not redis.call('HEGT' , key , goodsId) then
+if not redis.call('HGET' , key , goodsId) then
     return false
 end
 return true
